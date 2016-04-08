62b53bef861b17990d0ef1b7c76f801904dadd9f
Vladislav Prosvirnin <prosvirnin.vlad@gmail.com>
2016-04-08 22:15:42 +0300

    Создание потоков ядра читателей и писателей.

--- rwlkm.c
+++ rwlkm.c
@@ -1,13 +1,70 @@
 #include <asm/uaccess.h>
 #include <linux/module.h>
 #include <linux/vmalloc.h>
+#include <linux/kthread.h>
+
+/* Общий ресурс */
+static int *resource;
+
+/* Количество поток читателей и писателей */
+#define rwlkm_readers 4
+#define rwlkm_writers 4
+
+/* Указатели на потоки ядра */
+static struct task_struct *rwlkm_reader_handlers[rwlkm_readers];
+static struct task_struct *rwlkm_writer_handlers[rwlkm_writers];
+
+/* Функции потоков */
+static int rwlkm_reader(void* usrdata) {
+
+	while(!kthread_should_stop()) {}
+	return 0;
+
+}
+
+static int rwlkm_writer(void* usrdata) {
+
+	while(!kthread_should_stop()) {}
+	return 0;
+
+}
 
 static int __init rwlkm_init(void) {
+
+	int i = 0;
+
+	resource = (int *) vmalloc(sizeof(int));
+
+	if (!resource) {
+		return -ENOMEM;
+	}
+
+	for(i = 0; i < rwlkm_readers; i++) {
+		rwlkm_reader_handlers[i] = kthread_run(&rwlkm_reader, NULL, "rwlkm_reader");
+	}
+
+	for(i = 0; i < rwlkm_writers; i++) {
+		rwlkm_writer_handlers[i] = kthread_run(&rwlkm_writer, NULL, "rwlkm_writer");
+	}
+
 	return 0;
+
 }
 
 static void __exit rwlkm_exit(void) {
-	return;
+
+	int i = 0;
+
+	vfree(resource);
+
+	for(i = 0; i < rwlkm_readers; i++) {
+		kthread_stop(rwlkm_reader_handlers[i]);
+	}
+
+	for(i = 0; i < rwlkm_readers; i++) {
+		kthread_stop(rwlkm_writer_handlers[i]);
+	}
+
 }
 
 MODULE_LICENSE("GPL");
