529247c35e66901e5ee97ddfb6f932d9a6d7c235
Vladislav Prosvirnin <prosvirnin.vlad@gmail.com>
2016-04-09 08:51:39 +0300

    Блокировка читателей и писателей.

--- rwlkm.c
+++ rwlkm.c
@@ -3,13 +3,14 @@
 #include <linux/module.h>
 #include <linux/vmalloc.h>
 #include <linux/kthread.h>
+#include <linux/spinlock.h>
 
 /* Общий ресурс */
 static atomic_t *resource;
 
 /* Количество потоков читателей и писателей */
-#define rwlkm_readers 4
-#define rwlkm_writers 4
+#define rwlkm_readers 2
+#define rwlkm_writers 1
 
 /* Время блокировки потока (для снижения нагрузки на CPU) */
 #define rwlkm_sleep 2000
@@ -18,11 +19,19 @@ static atomic_t *resource;
 static struct task_struct *rwlkm_reader_handlers[rwlkm_readers];
 static struct task_struct *rwlkm_writer_handlers[rwlkm_writers];
 
+/* Spin - блокировка чтения - записи */
+DEFINE_RWLOCK(rwlkm_lock);
+
 /* Функции потоков */
 static int rwlkm_reader(void* usrdata) {
 
 	while(!kthread_should_stop()) {
+
+		read_lock(&rwlkm_lock);
+		printk("The reader has read the value: %d\n", atomic_read(resource));
+		read_unlock(&rwlkm_lock);
 		msleep(rwlkm_sleep);
+
 	}
 
 	return 0;
@@ -32,6 +41,10 @@ static int rwlkm_reader(void* usrdata) {
 static int rwlkm_writer(void* usrdata) {
 
 	while(!kthread_should_stop()) {
+		write_lock(&rwlkm_lock);
+		atomic_inc(resource);
+		printk("The writer wrote the value: %d\n", atomic_read(resource));
+		write_unlock(&rwlkm_lock);
 		msleep(rwlkm_sleep);
 	}
 	
@@ -48,6 +61,8 @@ static int __init rwlkm_init(void) {
 	if (!resource) {
 		return -ENOMEM;
 	}
+
+	atomic_set(resource, 0);
 
 	for(i = 0; i < rwlkm_readers; i++) {
 		rwlkm_reader_handlers[i] = kthread_run(&rwlkm_reader, NULL, "rwlkm_reader_%d", i);
